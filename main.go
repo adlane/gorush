8063fb3e1d9ec47b75f6cc24283f89040855178e
Adlane Achab <adlane.achab@gmail.com>
2020-01-18 17:08:30 +0100

    Use config file only.

--- main.go
+++ main.go
@@ -17,46 +17,16 @@ import (
 )
 
 func main() {
-	opts := config.ConfYaml{}
-
 	var (
 		ping        bool
 		showVersion bool
 		configFile  string
-		topic       string
-		message     string
-		token       string
-		title       string
 	)
 
 	flag.BoolVar(&showVersion, "version", false, "Print version information.")
 	flag.BoolVar(&showVersion, "v", false, "Print version information.")
 	flag.StringVar(&configFile, "c", "", "Configuration file path.")
 	flag.StringVar(&configFile, "config", "", "Configuration file path.")
-	flag.StringVar(&opts.Core.PID.Path, "pid", "", "PID file path.")
-	flag.StringVar(&opts.Ios.KeyPath, "i", "", "iOS certificate key file path")
-	flag.StringVar(&opts.Ios.KeyPath, "key", "", "iOS certificate key file path")
-	flag.StringVar(&opts.Ios.Password, "P", "", "iOS certificate password for gorush")
-	flag.StringVar(&opts.Ios.Password, "password", "", "iOS certificate password for gorush")
-	flag.StringVar(&opts.Android.APIKey, "k", "", "Android api key configuration for gorush")
-	flag.StringVar(&opts.Android.APIKey, "apikey", "", "Android api key configuration for gorush")
-	flag.StringVar(&opts.Core.Address, "A", "", "address to bind")
-	flag.StringVar(&opts.Core.Address, "address", "", "address to bind")
-	flag.StringVar(&opts.Core.Port, "p", "", "port number for gorush")
-	flag.StringVar(&opts.Core.Port, "port", "", "port number for gorush")
-	flag.StringVar(&token, "t", "", "token string")
-	flag.StringVar(&token, "token", "", "token string")
-	flag.StringVar(&opts.Stat.Engine, "e", "", "store engine")
-	flag.StringVar(&opts.Stat.Engine, "engine", "", "store engine")
-	flag.StringVar(&opts.Stat.Redis.Addr, "redis-addr", "", "redis addr")
-	flag.StringVar(&message, "m", "", "notification message")
-	flag.StringVar(&message, "message", "", "notification message")
-	flag.StringVar(&title, "title", "", "notification title")
-	flag.BoolVar(&opts.Android.Enabled, "android", false, "send android notification")
-	flag.BoolVar(&opts.Ios.Enabled, "ios", false, "send ios notification")
-	flag.BoolVar(&opts.Ios.Production, "production", false, "production mode in iOS")
-	flag.StringVar(&topic, "topic", "", "apns topic in iOS")
-	flag.StringVar(&opts.Core.HTTPProxy, "proxy", "", "http proxy url")
 	flag.BoolVar(&ping, "ping", false, "ping server")
 
 	flag.Usage = usage
@@ -80,40 +50,8 @@ func main() {
 		return
 	}
 
-	if opts.Ios.KeyPath != "" {
-		gorush.PushConf.Ios.KeyPath = opts.Ios.KeyPath
-	}
-
-	if opts.Ios.Password != "" {
-		gorush.PushConf.Ios.Password = opts.Ios.Password
-	}
-
-	if opts.Android.APIKey != "" {
-		gorush.PushConf.Android.APIKey = opts.Android.APIKey
-	}
-
-	if opts.Stat.Engine != "" {
-		gorush.PushConf.Stat.Engine = opts.Stat.Engine
-	}
-
-	if opts.Stat.Redis.Addr != "" {
-		gorush.PushConf.Stat.Redis.Addr = opts.Stat.Redis.Addr
-	}
-
-	// overwrite server port and address
-	if opts.Core.Port != "" {
-		gorush.PushConf.Core.Port = opts.Core.Port
-	}
-	if opts.Core.Address != "" {
-		gorush.PushConf.Core.Address = opts.Core.Address
-	}
-
 	if err = gorush.InitLog(); err != nil {
 		log.Fatalf("Can't load log module, error: %v", err)
-	}
-
-	if opts.Core.HTTPProxy != "" {
-		gorush.PushConf.Core.HTTPProxy = opts.Core.HTTPProxy
 	}
 
 	if gorush.PushConf.Core.HTTPProxy != "" {
@@ -131,89 +69,8 @@ func main() {
 		return
 	}
 
-	// send android notification
-	if opts.Android.Enabled {
-		gorush.PushConf.Android.Enabled = opts.Android.Enabled
-		req := gorush.PushNotification{
-			Platform: gorush.PlatFormAndroid,
-			Message:  message,
-			Title:    title,
-		}
-
-		// send message to single device
-		if token != "" {
-			req.Tokens = []string{token}
-		}
-
-		// send topic message
-		if topic != "" {
-			req.To = topic
-		}
-
-		err := gorush.CheckMessage(req)
-
-		if err != nil {
-			gorush.LogError.Fatal(err)
-		}
-
-		if err := gorush.InitAppStatus(); err != nil {
-			return
-		}
-
-		gorush.PushToAndroid(req)
-
-		return
-	}
-
-	// send ios notification
-	if opts.Ios.Enabled {
-		if opts.Ios.Production {
-			gorush.PushConf.Ios.Production = opts.Ios.Production
-		}
-
-		gorush.PushConf.Ios.Enabled = opts.Ios.Enabled
-		req := gorush.PushNotification{
-			Platform: gorush.PlatFormIos,
-			Message:  message,
-			Title:    title,
-		}
-
-		// send message to single device
-		if token != "" {
-			req.Tokens = []string{token}
-		}
-
-		// send topic message
-		if topic != "" {
-			req.Topic = topic
-		}
-
-		err := gorush.CheckMessage(req)
-
-		if err != nil {
-			gorush.LogError.Fatal(err)
-		}
-
-		if err := gorush.InitAppStatus(); err != nil {
-			return
-		}
-
-		if err := gorush.InitAPNSClient(); err != nil {
-			return
-		}
-		gorush.PushToIOS(req)
-
-		return
-	}
-
 	if err = gorush.CheckPushConf(); err != nil {
 		gorush.LogError.Fatal(err)
-	}
-
-	if opts.Core.PID.Path != "" {
-		gorush.PushConf.Core.PID.Path = opts.Core.PID.Path
-		gorush.PushConf.Core.PID.Enabled = true
-		gorush.PushConf.Core.PID.Override = true
 	}
 
 	if err = createPIDFile(); err != nil {
@@ -257,27 +114,9 @@ var usageStr = `
 Usage: gorush [options]
 
 Server Options:
-    -A, --address <address>          Address to bind (default: any)
-    -p, --port <port>                Use port for clients (default: 8088)
     -c, --config <file>              Configuration file path
-    -m, --message <message>          Notification message
-    -t, --token <token>              Notification token
-    -e, --engine <engine>            Storage engine (memory, redis ...)
-    --title <title>                  Notification title
-    --proxy <proxy>                  Proxy URL
-    --pid <pid path>                 Process identifier path
-    --redis-addr <redis addr>        Redis addr (default: localhost:6379)
     --ping                           healthy check command for container
-iOS Options:
-    -i, --key <file>                 certificate key file path
-    -P, --password <password>        certificate key password
-    --ios                            enabled iOS (default: false)
-    --production                     iOS production mode (default: false)
-Android Options:
-    -k, --apikey <api_key>           Android API Key
-    --android                        enabled android (default: false)
 Common Options:
-    --topic <topic>                  iOS or Android topic message
     -h, --help                       Show this message
     -v, --version                    Show version
 `
